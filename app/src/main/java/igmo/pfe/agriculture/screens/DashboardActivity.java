babb4ef01be78b2f95fbfb4636089020460484a0
TidelxD <luffygear37@gmail.com>
2022-04-21 21:46:00 +0000

    - Add DashboardActivity View ( XML )

---
+++ app/src/main/java/igmo/pfe/agriculture/screens/DashboardActivity.java
@@ -0,0 +1,16 @@
+package igmo.pfe.agriculture.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import igmo.pfe.agriculture.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
